8183cdf7f53e76749b634b58c403b5602278c681
HayyanAl-Shaar <119664122+HayyanAl-Shaar@users.noreply.github.com>
2022-12-02 17:03:15 +0200

    1 file modified

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
 const btn = document.querySelectorAll('.btn');
 const player = document.getElementsByClassName('player')[0];
 const computer = document.getElementsByClassName('computer')[0];
-const random = Math.floor(Math.random() * 3);
+const playerScore = document.getElementsByClassName('player-score')[0]
+const computerScore = document.getElementsByClassName('computer-score')[0]
 const computerArray = ['rock', 'paper', 'scissors'];
 for (let i = 0; i < btn.length; i++) {
   btn[i].addEventListener('click', () => {
     player.innerHTML = btn[i].innerHTML;
     setTimeout(() => {
+      const random = Math.floor(Math.random() * 3);
       computer.innerHTML = computerArray[random];
     }, 500);
   });
+}
+
+
+const gamelogic = ()=> {
+  if((player.innerHTML === 'paper' )
+
 }
