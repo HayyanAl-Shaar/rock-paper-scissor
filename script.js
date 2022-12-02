191fe35ec77ef4ac6ea95a7def85b7434b3d89fe
HayyanAl-Shaar <119664122+HayyanAl-Shaar@users.noreply.github.com>
2022-12-02 13:47:47 +0200

    2 files modified

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log('Hello!');
+const rock = document.getElementsByClassName('rock')[0];
+const player = document.getElementsByClassName('player')[0];
+const computer = document.getElementsByClassName('computer')[0];
+const random = Math.floor(Math.random() * 3);
+const computerArray = ['rock', 'paper', 'scissors'];
+rock.addEventListener('click', () => {
+  player.innerHTML = 'rock';
+  setTimeout(() => {
+    computer.innerHTML = computerArray[random];
+  }, 500);
+});
