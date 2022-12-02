f6884842d8c91b33210c4efc22759c0617db19eb
HayyanAl-Shaar <119664122+HayyanAl-Shaar@users.noreply.github.com>
2022-12-02 17:21:20 +0200

    2 files modified

--- script.js
+++ script.js
@@ -1,21 +1,32 @@
+// ||
+
 const btn = document.querySelectorAll('.btn');
 const player = document.getElementsByClassName('player')[0];
 const computer = document.getElementsByClassName('computer')[0];
-const playerScore = document.getElementsByClassName('player-score')[0]
-const computerScore = document.getElementsByClassName('computer-score')[0]
-const computerArray = ['rock', 'paper', 'scissors'];
+const playerScore = document.getElementsByClassName('player-score')[0];
+const computerScore = document.getElementsByClassName('computer-score')[0];
+const computerArray = ['rock', 'paper', 'scissores'];
 for (let i = 0; i < btn.length; i++) {
   btn[i].addEventListener('click', () => {
     player.innerHTML = btn[i].innerHTML;
     setTimeout(() => {
       const random = Math.floor(Math.random() * 3);
       computer.innerHTML = computerArray[random];
+      gamelogic();
     }, 500);
   });
 }
 
-
-const gamelogic = ()=> {
-  if((player.innerHTML === 'paper' && computer.innerHTML === 'rock')||  )
-
-}
+const gamelogic = () => {
+  if (
+    (player.innerHTML === 'paper' && computer.innerHTML === 'rock') ||
+    (player.innerHTML === 'rock' && computer.innerHTML === 'scissores') ||
+    (player.innerHTML === 'scissores' && computer.innerHTML === 'paper')
+  ) {
+    playerScore.innerHTML = Number(playerScore.innerHTML) + 1;
+    console.log(playerScore.innerHTML);
+  } else if (player.innerHTML === computer.innerHTML) {
+  } else {
+    computerScore.innerHTML = Number(computerScore.innerHTML) + 1;
+  }
+};
